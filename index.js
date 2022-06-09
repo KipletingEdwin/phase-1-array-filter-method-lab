009c3d40f5942af4b679b1b307d03510c30679f4
Kipleting <kipletingedwin4@gmail.com>
2022-06-09 23:19:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+//declare a function that return all drivers
+
+function findMatching(driversName,details){
+    return driversName.filter(function(driversName)
+    {return driversName.toLowerCase()===details.toLowerCase()})
+
+}
+//declare a function that return driver if letters match
+
+function fuzzyMatch(driversName,details){
+    return driversName.filter(function(driversName)
+    {return driversName.toLowerCase().substring(0,details.lenght)===details.toLowerCase()})
+}
+
+//declare another function 
+
+function matchName(driversName,details){
+    return driversName.filter(function (driversName) 
+    { return driversName.name === details })
+}
